7cfa4afdd9c4db21aa80b6d263f44e3d926e5d57
dokdek <dokdek@gmail.com>
2021-04-19 14:14:10 -0400

    Changed onhover colour to purple

--- components/lesson-card.js
+++ components/lesson-card.js
@@ -1,6 +1,6 @@
 export default function LessonCard({number}){
     return(
-        <button className="flex flex-col justify-items-center items-center rounded-lg shadow-xl bg-purple-600 py-10 px-32 space-y-3 hover:bg-purple-500">
+        <button className="flex flex-col justify-center items-center rounded-lg shadow-xl bg-purple-600 py-10 px-32 space-y-3 hover:bg-purple-500">
             <h1 className="text-white font-bold">Lessons</h1>
             <p className="rounded-full bg-white px-3 py-1">2</p>
         </button>
