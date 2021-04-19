d1a7ed1a7b785d04532658ccd8da95c575e17fe2
dokdek <dokdek@gmail.com>
2021-04-19 14:02:02 -0400

    Added lessons and review cards

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,16 @@
 import Head from 'next/head'
 import Navbar from "../components/navbar";
+import ReviewCard from "../components/review-card";
+import LessonCard from "../components/lesson-card";
 
 export default function Home() {
   return (
+    <div>
     <Navbar/>
+    <div className="lessons-div flex flex-col md:flex-row justify-center items-center space-y-3 md:space-x-3 md:space-y-0 my-10">
+    <ReviewCard/>
+    <LessonCard/>
+    </div>
+    </div>
   )
 }
