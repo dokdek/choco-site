fbd992b8b15e03d8d13c0d3bdc064797a9f631b6
dokdek <dokdek@gmail.com>
2021-04-19 23:24:41 -0400

    Initial commit

---
+++ components/vocab-card.js
@@ -0,0 +1,10 @@
+
+
+export default function VocabCard(){
+    return(
+        <button className="flex flex-col px-10 py-6 bg-purple-600 rounded m-1 hover:bg-purple-500">
+            <p className="font-bold text-white text-xl">元気</p>
+            <p className="font-bold text-white">Healthy</p>
+        </button>
+    )
+}
