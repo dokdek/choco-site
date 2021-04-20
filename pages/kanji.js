8751142ff697972ea03b4f97be9a82dbaff12a5b
dokdek <dokdek@gmail.com>
2021-04-19 22:34:24 -0400

    Initial commit

---
+++ pages/kanji.js
@@ -0,0 +1,17 @@
+import Navbar from "../components/navbar";
+
+export default function Kanji() {
+  return (
+    <div className="bg-gray-800 min-h-screen">
+      <Navbar />
+      <div className="main-container flex flex-col justify-center items-center">
+      <div class="pt-2 relative mx-auto text-gray-600">
+        <input class="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
+          type="search" name="search" placeholder="Search"></input>
+        {//<button type="submit" class="absolute right-0 top-0 mt-5 mr-4"></button>
+}
+      </div>
+      </div>
+    </div>
+  );
+}
