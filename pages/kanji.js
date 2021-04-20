4b60fc332a279a017c68bcc7b1fe7acebe36dda1
dokdek <dokdek@gmail.com>
2021-04-19 22:55:31 -0400

    Added test kanji cards and mock search bar

--- pages/kanji.js
+++ pages/kanji.js
@@ -1,16 +1,33 @@
 import Navbar from "../components/navbar";
+import KanjiCard from "../components/kanji-card";
 
 export default function Kanji() {
+    function testPrint(obj) {
+        let result = [];
+        for (let i = 0; i < 60; i++) {
+          result.push(obj);
+        }
+        return result;
+      }
   return (
     <div className="bg-gray-800 min-h-screen">
       <Navbar />
       <div className="main-container flex flex-col justify-center items-center">
-      <div class="pt-2 relative mx-auto text-gray-600">
-        <input class="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
-          type="search" name="search" placeholder="Search"></input>
-        {//<button type="submit" class="absolute right-0 top-0 mt-5 mr-4"></button>
-}
-      </div>
+        <div class="flex flex-wrap flex-col md:flex-row justify-start w-3/4 py-4 text-gray-600">
+        <h1 className="pr-5 font-bold text-2xl text-white pt-1">All Kanji</h1>
+          <input
+            class="border-2 border-gray-300 bg-white h-10 w-3/4 max-w-md px-5 pr-16 rounded-lg text-sm focus:outline-none"
+            type="search"
+            name="search"
+            placeholder="Search"
+          ></input>
+          {
+            //<button type="submit" class="absolute right-0 top-0 mt-5 mr-4"></button>
+          }
+        </div>
+        <div className="flex flex-wrap flex-row justify-start w-3/4">
+          {testPrint(<KanjiCard/>)}
+        </div>
       </div>
     </div>
   );
