279224387e2192b9094d148d2ae37dcc811ec648
dokdek <dokdek@gmail.com>
2021-04-19 20:06:50 -0400

    Initial commit

---
+++ components/vocab-mini-card.js
@@ -0,0 +1,9 @@
+
+
+export default function VocabMiniCard(){
+    return(
+        <div className="px-5 py-2 bg-purple-600 rounded font-bold text-white m-1">
+            元気
+        </div>
+    )
+}
