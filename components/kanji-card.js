3afbe4aa75da5f45a74a67dc943f83d9df5f531c
dokdek <dokdek@gmail.com>
2021-04-19 22:55:12 -0400

    Initial commit

---
+++ components/kanji-card.js
@@ -0,0 +1,10 @@
+
+
+export default function KanjiCard(){
+    return(
+        <button className="flex flex-col px-10 py-6 bg-pink-600 rounded m-1 hover:bg-pink-500">
+            <p className="font-bold text-white text-xl">六</p>
+            <p className="font-bold text-white">Six</p>
+        </button>
+    )
+}
