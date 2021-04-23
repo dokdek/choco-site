05e19ac06e6fb9ac74279c6830ad18ec67f7410b
dokdek <dokdek@gmail.com>
2021-04-22 23:23:43 -0400

    updated to include link to review page

--- components/review-card.js
+++ components/review-card.js
@@ -1,10 +1,13 @@
 
+import Link from "next/link";
 
 export default function ReviewCard({number}){
     return(
-        <button className="flex flex-col justify-center items-center rounded-lg shadow-xl bg-pink-600 py-10 px-32 space-y-3 hover:bg-pink-500">
+        <Link href="/review">
+        <a className="flex flex-col justify-center items-center rounded-lg shadow-xl bg-pink-600 py-10 px-32 space-y-3 hover:bg-pink-500">
             <h1 className="text-white font-bold">Reviews</h1>
             <p className="rounded-full bg-white px-3 py-1">3</p>
-        </button>
+        </a>
+        </Link>
     );
 }
